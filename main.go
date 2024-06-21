25d1eb2ee8cfbaa24841b96893c20c1a92f15c91
Ananta Krishnan B <anantukid@gmail.com>
2024-06-21 23:13:54 +0530

    initial commit

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+import (
+    "flag"
+)
+
+func scan(path string) {
+    print("scan")
+}
+
+func stats(email string) {
+    print("stats")
+}
+
+func main() {
+    var folder string
+    var email string
+    flag.StringVar(&folder, "add", "", "add a new folder to scan for Git repositories")
+    flag.StringVar(&email, "email", "anantukid@gmail.com", "the email to scan")
+    flag.Parse()
+    
+    if folder != "" {
+        scan(folder)
+        return
+    }
+
+    stats(email)
+}
