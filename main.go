dee5ecd1477a22d6290d73bec798868ad28ab750
Ananta Krishnan B <anantukid@gmail.com>
2024-08-15 15:46:43 +0530

    feat: added some more functions and restructured project

--- main.go
+++ main.go
@@ -1,72 +1,7 @@
 package main
 import (
     "flag"
-	"fmt"
-	"log"
-	"os"
-	"os/user"
-	"strings"
 )
-
-func scanGitFolders(folders []string, folder string) []string{    
-    folder = strings.TrimSuffix(folder, "/")
-
-    f, err := os.Open(folder)
-    if err!=nil{
-        log.Fatal(err)
-    }
-    files, err := f.Readdir(-1)
-    f.Close()
-    if err!=nil{
-        log.Fatal(err)
-    }
-
-    var path string
-
-    for _, file := range files{
-        if file.IsDir(){
-            path = folder + "/" + file.Name()
-            if file.Name() == ".git"{
-                path = strings.TrimSuffix(path, "/.git")
-                fmt.Println(path)
-                folders = append(folders,path)
-                continue
-            }
-            if file.Name() == "vendor" || file.Name() == "node_modules"{
-                continue
-            }
-            folders = scanGitFolders(folders, path)
-        }
-    }
-    return folders
-}
-
-func recursiveScanFolder(folder string) []string{
-    return scanGitFolders(make([]string, 0), folder)
-}
-
-func getDotFilePath() string{
-    usr, err := user.Current()
-    if err != nil{
-        log.Fatal(err)
-    }
-
-    dotFile := usr.HomeDir + "/.gogitlocalstats"
-
-    return dotFile
-}
-
-func scan(folder string){
-    fmt.Printf("Found folders:\n\n")
-    repositories := recursiveScanFolder(folder)
-    filePath := getDotFilePath()
-    addNewSliceElementsToFile(filePath, repositories) // to be added
-    fmt.Printf("\n\nSuccessfully added\n\n")
-}
-
-func stats(email string) {
-    print("stats")
-}
 
 func main(){
     var folder string
